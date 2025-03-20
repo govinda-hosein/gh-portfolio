04811a9b89192fd5673a7fd270e7f81a4a3766c5
Govinda Hosein <raziel619dev@gmail.com>
2025-03-20 14:57:36 -0400

    Add react to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://govindahosein.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   markdown: {
     shikiConfig: {
       theme: "night-owl",
