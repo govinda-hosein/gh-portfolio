183850a288b68bd81f25ab050f23d6f797c13c77
Govinda Hosein <raziel619dev@gmail.com>
2025-03-26 11:47:05 -0400

    Add framer motion

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "src/motion";
+
+function ServiceCard() {
+  return (
+    <motion.div
+      variants={fadeIn("right", "spring", 0.5 * 1, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <h3 className="text-white text-[20px] text-center font-bold">Title</h3>
+      </div>
+    </motion.div>
+  );
+}
+
+export default ServiceCard;
