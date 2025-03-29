f1151f374e53c766556a1b09af71aaa3468a9aa8
Govinda Hosein <raziel619dev@gmail.com>
2025-03-29 15:35:38 -0400

    Update service card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -8,11 +8,11 @@ function ServiceCard({ index, title, icon }) {
     <div className="w-[250px]">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
-        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        className="w-full green-pink-gradient p-1 rounded-[20px] shadow-card"
       >
         <div
           options={{ max: 45, scale: 1, speed: 450 }}
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          className="bg-background rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
           <h4 className="text-white text-[20px] text-center font-bold">
