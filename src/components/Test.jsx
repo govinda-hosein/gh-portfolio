511689ac66f16b0ac729f24bd10253635d218152
Govinda Hosein <raziel619dev@gmail.com>
2025-03-30 15:24:53 -0400

    Add timeline package

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,15 +1,31 @@
-import { motion } from "framer-motion";
+import React from "react";
+import "react-vertical-timeline-component/style.min.css";
 
-const Card1 = () => {
+// Import as a default module to avoid CommonJS issues
+import pkg from "react-vertical-timeline-component";
+
+const VerticalTimeline = pkg.VerticalTimeline;
+const VerticalTimelineElement = pkg.VerticalTimelineElement;
+
+function Card1() {
   return (
-    <>
-      <motion.div
-        className="bg-red-400 w-[10px] h-[10px]"
-        animate={{ x: 1000 }}
-      />
-      <h1>yo bros</h1>
-    </>
+    <VerticalTimeline>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
+        contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+        contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+        date="2011 - present"
+        iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+      >
+        <h3 className="vertical-timeline-element-title">Creative Director</h3>
+        <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+        <p>
+          Creative Direction, User Experience, Visual Design, Project
+          Management, Team Leading
+        </p>
+      </VerticalTimelineElement>
+    </VerticalTimeline>
   );
-};
+}
 
 export default Card1;
