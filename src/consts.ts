5285da2ea27abdf436395fd3ce3d67751a74e357
Govinda Hosein <raziel619dev@gmail.com>
2025-03-30 15:39:57 -0400

    Add incomplete Timeline component

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,64 @@
 export const SITE_TITLE = "Govinda Hosein";
 export const SITE_DESCRIPTION = "Welcome to my site!";
 
+const EXPERIENCES = [
+  {
+    title: "ICT Technologist",
+    company_name: "Caribbean Industrial Research Institute",
+    icon: "/icons/web.png",
+    iconBg: "#FFFFFF",
+    date: "September 2014 - September 2017",
+    points: [
+      "Team member of the Data Innovations Solutions Centre (DISC)",
+      "Provided IT support to the Centre for Enterprise Development (CED)",
+      "Delivered numerous programming workshops at the Microsoft Innovation Centre (MIC)",
+      "Maintained all company websites",
+      "Supervised the development of an Electronic Health Record (EHR) system",
+      "Developed the prototype and supported subsequent release of the Hyperglycemia in Pregnancy Trinidad and Tobago (HiPTT) online platform",
+    ],
+  },
+  {
+    title: "Associate Professional",
+    company_name: "Department of Electrical and Computer Engineering, UWI",
+    icon: "/icons/web.png",
+    iconBg: "#FFFFFF",
+    date: "February 2019 - February 2020",
+    points: [
+      "Designed and supervised the installation of the first Electric Vehicle (EV) charging station for the University of the West Indies (UWI), St. Augustine campus",
+      "Wrote and submitted 6 research papers for publication in the field of renewable energy, EVs and Machine Learning (ML)",
+      "Delivered training workshops for power system modelling in the PowerFactory software",
+      "Facilitated an external PhD study by partially modelling Trinidad and Tobago's transmission and distribution system",
+    ],
+  },
+  {
+    title: "Electrical and Computer Engineer",
+    company_name: "FaSoVe",
+    icon: "/icons/web.png",
+    iconBg: "#FFFFFF",
+    date: "February 2020 - October 2020",
+    points: [
+      "Designed, developed and deployed the ProTool; an online assessment productivity measurement tool for the National Competitiveness and Producitivty Council (NCPC) of St. Lucia.",
+      "Performed code refactoring and visual updates to the Percussion Harmonic Instrument (P.H.I.) table app",
+      "Designed, developed and deployed and online platform to remotely monitor boats",
+    ],
+  },
+  {
+    title: "Software Development and Design Engineer",
+    company_name: "Digicel",
+    icon: "/icons/web.png",
+    iconBg: "#FFFFFF",
+    date: "November 2020 - Present",
+    points: [
+      "A core developer of the MyDigicel App. Built in flutter and published to both google play and ios stores. Has more than 10 million downloads",
+      "Designed, developed and deployed DigiAssist, a platform for both customers and internal agents to troubleshoot modem issues with an automated flow. UI/UX designs were created in figma",
+      "Implemented responsive design and ensured cross-browser compatibility",
+      "Automated manual tasks across the entire company using primarily python",
+      "Delivered",
+      "Effectively deployed numerous web applications to meet business needs",
+    ],
+  },
+];
+
 export const SERVICES = [
     {
       title: "Full Stack Web Developer",
